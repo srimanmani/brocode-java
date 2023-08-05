4a69a4e8758e5f61e35b09df32b3830aa3b0c8b1
muthu <sriman14092003@gmail.com>
2023-08-05 08:00:38 +0530

    gui

--- src/variable.java
+++ src/variable.java
@@ -5,7 +5,11 @@ public class variable
         String name=JOptionPane.showInputDialog("enter your name");
         JOptionPane.showMessageDialog(null,"hello"+name);
 
+        int age=Integer.parseInt(JOptionPane.showInputDialog("enter your age"));
+        JOptionPane.showMessageDialog(null,"you are"+age+"years old");
 
+        Double height=Double.parseDouble(JOptionPane.showInputDialog("enter your height"));
+        JOptionPane.showMessageDialog(null,"you are"+height+"cm tall");
     }
 
 }
